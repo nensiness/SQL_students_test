3e2b20b7917ee46d89073cfc86642484199212f9
nensiness <probst_em@mail.ru>
2024-10-12 02:58:02 +0500

    sql_requests.sql

--- sql_requests.sql
+++ sql_requests.sql
@@ -8,3 +8,11 @@ SELECT Country, COUNT(Country)
 FROM Students
 Group By Country;
 --Группировка по количеству каждой страны в данном столбце(Страна);
+
+SELECT AVG(Score) AS Среднее FROM Progress WHERE StudentID=1 ;
+--Находим среднее значение колонки Score Таблицы Progress по студенту с ID-1
+
+Или
+
+SELECT SUM(Score)/ COUNT(Score)  AS Среднее FROM Progress WHERE StudentID=1 ;
+--Находим среднее значение колонки Score Таблицы Progress по студенту с ID-1
