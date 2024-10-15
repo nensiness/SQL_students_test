5231e8617410a28cbc670c1f041298bb8e6c8f5f
nensiness <probst_em@mail.ru>
2024-10-15 22:38:36 +0500

    Update Join_requests.sql

--- Join_requests.sql
+++ Join_requests.sql
@@ -17,6 +17,9 @@ SELECT
   Class_rooms.ClassroomID=Progress.ClassroomID; -- Соединяем по идентификатору класса
 
 
+
+
+
 --Объединение двух таблиц INNER JOIN
 
 
@@ -24,9 +27,9 @@ SELECT
 
 CREATE TABLE Persons
 (
-	PersonID INTEGER PRIMARY KEY,
-	First_name VARCHAR(255) NOT NULL,
-	Position_ref INEGER NULL
+	PersonID INTEGER PRIMARY KEY, -- Уникальный идентификатор для каждого сотрудника
+	First_name VARCHAR(255) NOT NULL, -- Имя сотрудника, не может быть NULL
+	Position_ref INEGER NULL -- Ссылка на идентификатор позиции (может быть NULL)
 );
 
 
@@ -38,12 +41,13 @@ VALUES
 	('Ivan', 1);
 
 
+
 --Создаем и заполняем данными вторую таблицу Positions
 
 CREATE TABLE Positions
 (
-	PositionID INTEGER PRIMARY KEY,
-	Position VARCHAR(255) NOT NULL
+	PositionID INTEGER PRIMARY KEY, -- Уникальный идентификатор для каждой позиции
+	Position VARCHAR(255) NOT NULL -- Название позиции, не может быть NULL
 );
 
 
@@ -57,7 +61,7 @@ VALUES
 
 --Объединяем таблицы 
 
-SELECT PersonID, First_name, PositionID, Position -- Выбираем идентификатор, имя сотрудника, ID позиции и название позиции
+SELECT PersonID, First_name, PositionID, Position 
 FROM Persons --Указываем основную таблицу Persons, из которой берутся данные о сотрудниках. Мы собираемся объединить ее с таблицей Positions.
 INNER JOIN Positions ON PositionID=Position_ref -- Соединяем таблицы по совпадению ID позиции (PositionID = Position_ref)
 
@@ -65,4 +69,29 @@ INNER JOIN Positions ON PositionID=Position_ref -- Соединяем табли
 -- Если сотрудник добавлен без позиции (Position_ref), его запись не попадет в результат, так как INNER JOIN исключает несоответствия.
 
 
+	
+
+
+--Объединение LEFT и RIGHT JOIN
+
+--LEFT JOIN** и RIGHT JOIN отличаются от INNER JOIN тем, что к итоговой таблице добавляются строки не имеющие совпадений в соседней таблице.
+
+--Если используется LEFT JOIN, добавляются все записи из таблицы указанной по левую сторону от оператора, если RIGHT JOIN, то из таблицы по правую сторону от оператора.
+
+
+SELECT PersonID, First_name, Position, PositionID 
+FROM Persons 
+LEFT JOIN Positions ON PositionID = Position_ref  -- Соединяем таблицы, включая все строки из Persons, даже если нет совпадений в Positions
+	
+-- Результат: возвращаются все сотрудники из таблицы Persons, даже если у них нет позиции. 
+-- Для таких сотрудников Position и PositionID будут NULL. PersonID будет показывать соответствующий ID из таблицы Persons.
+
+
+
+SELECT PersonID, First_name, Position, PositionID 
+FROM Persons 
+RIGHT JOIN Positions ON PositionID = Position_ref  -- Соединяем таблицы, включая все строки из таблицы Positions, даже если нет совпадений в Persons
+
+-- Результат: возвращаются все позиции из таблицы Positions, даже если у них нет соответствующего сотрудника. 
+-- Для таких позиций PersonID и First_name будут NULL, PositionID будет показывать соответствующий ID из таблицы Positions.
 
